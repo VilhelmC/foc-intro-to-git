129a7351b765677d07c3a863d5e2340a1de8750c
Vilhelm Carlström <vilhelm.carlstrom@gmail.com>
2022-03-17 14:44:19 +0100

    Added VilhelmC()

---
+++ test_module/VilhelmC.py
@@ -0,0 +1,13 @@
+class VilhelmC():
+    def __init__(self):
+        self.height = 1860
+        self.weight = 72
+        self.age = 30
+        self.description = "Mmm"
+
+    def function(self):
+        return 1 + 1
+
+if __name__=="__main__":
+    print("Yes?")
+    pass
